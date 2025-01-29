59fdd4d80d9047fdf514e149f272466e6d5c2564
Bayu Rizki Rahmadi <bayu.r.rahmadi@gmail.com>
2025-01-29 11:18:05 +0700

    chore: add jsdoc to useInfiniteScroll hooks

--- src/hooks/useInfiniteScroll.ts
+++ src/hooks/useInfiniteScroll.ts
@@ -1,3 +1,26 @@
+/**
+ * A custom hook for implementing infinite scroll functionality.
+ * This hook manages pagination of data and provides methods to load more items as needed.
+ *
+ * @template T - The type of items in the data array
+ *
+ * @param {Object} props - The hook parameters
+ * @param {T[]} props.data - The complete array of items to be paginated
+ * @param {number} props.itemsPerPage - Number of items to display per page
+ *
+ * @returns {Object} An object containing:
+ *   - displayedData: The current subset of data being displayed
+ *   - hasMore: Boolean indicating if there are more items to load
+ *   - loadMore: Function to trigger loading the next page of items
+ *
+ * @example
+ * ```typescript
+ * const { displayedData, hasMore, loadMore } = useInfiniteScroll({
+ *   data: items,
+ *   itemsPerPage: 10
+ * });
+ * ```
+ */
 import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface UseInfiniteScrollProps<T> {
